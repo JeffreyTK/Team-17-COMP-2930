e79a5835bdb9ed6ae8f8974b177861b3362cacb4
Irene Hsieh <hsiehiisuan@gmail.com>
2019-05-02 15:37:02 -0700

    pulled vue file

---
+++ barbuddies/src/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="profile">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'Profile',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
