fb8bc4eb2864e17de0f3934ded47347d70735c68
Luke Hansen <lukejohnhansen@gmail.com>
2019-05-03 12:48:03 -0700

    testing liveshare vs code extension

--- barbuddies/src/components/LoginSignup.vue
+++ barbuddies/src/components/LoginSignup.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
-    <div>
-    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+  <div id="LoginSignup">
+    <div id="login">
+      <p>fucc</p>
+      <p>fucc ME</p>
+      <b-button v-b-modal.modal-1>Login</b-button>
 
-    <b-modal id="modal-1" title="BootstrapVue" ok-variant="submit">
-      <p class="my-4">Hello from modal!</p>
-      <form>
-        <label>Type something</label><input type="text">
-      </form>
-    </b-modal>
+      <b-modal id="modal-1" title="BootstrapVue" ok-variant="submit">
+        <p class="my-4">Hello from modal!</p>
+        <form>
+          <label>Type something</label><input type="text">
+        </form>
+      </b-modal>
 
-</div>
-    <a href="">Signup</a>
+    <div id="signup">
+    <b-button v-b-modal.modal-1>Signup</b-button>
+
+        <b-modal id="modal-1" title="BootstrapVue" ok-variant="submit">
+          <p class="my-4">Hello from modal!</p>
+          <form>
+            <label>Type something</label><input type="text">
+          </form>
+        </b-modal>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -20,8 +31,5 @@ export default {
 }
 </script>
 <style scoped>
-  div {
-    position: absolute;
-    bottom: 0px;
-  }
+
 </style>
