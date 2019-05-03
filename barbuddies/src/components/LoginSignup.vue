ba8635115a6978fbdde678ffc91b566717cda742
Luke Hansen <lukejohnhansen@gmail.com>
2019-05-03 11:58:45 -0700

    Added bootstrap to landing page

--- barbuddies/src/components/LoginSignup.vue
+++ barbuddies/src/components/LoginSignup.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <a href="">Login</a>
+    <div>
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+    <b-modal id="modal-1" title="BootstrapVue" ok-variant="submit">
+      <p class="my-4">Hello from modal!</p>
+      <form>
+        <label>Type something</label><input type="text">
+      </form>
+    </b-modal>
+
+</div>
     <a href="">Signup</a>
   </div>
 </template>
@@ -10,5 +20,8 @@ export default {
 }
 </script>
 <style scoped>
-
+  div {
+    position: absolute;
+    bottom: 0px;
+  }
 </style>
