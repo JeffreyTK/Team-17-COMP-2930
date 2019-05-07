ba059b5a49d785c72c1c6c0ccacfb9357774ac38
JeffreyK <jeffrey_kuo113@hotmail.com>
2019-05-07 11:20:03 -0700

    updated group and user javascripts with the proper syntax

--- barbuddies/API/Groups.js
+++ barbuddies/API/Groups.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const GroupsSchema = Schema(
+const GroupsSchema =  new Schema(
     {
-        GroupID:{type:String, required = true},
-        UserID:{type:Array, required = true}
+        GroupID: String,
+        UserID: Array
     }
 );
-const Groups = mongoose.model("Groups", UserSchema);
+const Groups = mongoose.model("Groups", GroupsSchema);
 module.exports = Groups;
--- barbuddies/API/User.js
+++ barbuddies/API/User.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const UserSchema = Schema(
+const UserSchema = new Schema(
     {
-        firstName: {type: String, required = true},
-        lastName: {type: String, required = true},
-        DOB:{type: String, required = true},
-        gender:{type: String, required = true},
-        weight:{type: Number, required = true},
-        height:{type: String, required = true},
-        email:{type: String, required = true},
-        password:{type: String, required = true},
-        userID:{type:String, required = true},
-        groupID:{type: String, required = true}
+        firstName: String,
+        lastName: String,
+        DOB: String,
+        gender: String,
+        weight: Number,
+        height: String,
+        email: String,
+        password: String,
+        userID: String,
+        groupID: Array
     }
 );
 const User = mongoose.model("User", UserSchema);
