e90b65cfdb597c11047b1870d7fa47429049a61d
JeffreyK <jeffrey_kuo113@hotmail.com>
2019-05-08 15:08:47 -0700

    updated core files

--- barbuddies/src/components/LoginSignup.vue
+++ barbuddies/src/components/LoginSignup.vue
@@ -1,37 +1,44 @@
 <template>
-  <div id="LoginSignup">
-    <div id="login">
-      <b-button v-b-modal.modal-1>Login</b-button>
+  <div>  
+    <ImageSlider />
+    <div id="LoginSignup">
+      <div id="login">
+        <b-button v-b-modal.modal-1>Login</b-button>
 
-      <b-modal id="modal-1" title="Bar Buddies" ok-title="Login">
-        <p class="my-4">Login</p>
-          <input  placeholder="Email">
-          <input type="password" placeholder="Password">
-      </b-modal>
+        <b-modal id="modal-1" title="Bar Buddies" ok-title="Login">
+          <p class="my-4">Login</p>
+            <input  placeholder="Email">
+            <input type="password" placeholder="Password">
+        </b-modal>
 
-    <div id="signup">
-    <b-button v-b-modal.modal-2 @click = "onClick" >Signup</b-button>
-        <b-modal id="modal-2" title="Bar Buddies" ok-title="Signup">
-        <p class="my-4">Sign-up</p>
-          <input v-model = "firstName" placeholder="First Name">
-          <input v-model = "lastName" placeholder="Last Name">
-          <input v-model = "email"  placeholder="Email">
-          <input v-model = "password"  placeholder="Password">
-          <input v-model = "DOB"  placeholder="Date Of Birth">
-          <input v-model = "gender"  placeholder="Gender">
-          <input v-model = "height"  placeholder="Height">
-          <input v-model = "weight"  placeholder="Weight">
-          <input type="password" placeholder="Confirm Password">
-      </b-modal>
+      <div id="signup">
+      <b-button v-b-modal.modal-2 @click = "onClick" >Signup</b-button>
+          <b-modal id="modal-2" title="Bar Buddies" ok-title="Signup">
+          <p class="my-4">Sign-up</p>
+            <input v-model = "firstName" placeholder="First Name">
+            <input v-model = "lastName" placeholder="Last Name">
+            <input v-model = "email"  placeholder="Email">
+            <input v-model = "password"  placeholder="Password">
+            <input v-model = "DOB"  placeholder="Date Of Birth">
+            <input v-model = "gender"  placeholder="Gender">
+            <input v-model = "height"  placeholder="Height">
+            <input v-model = "weight"  placeholder="Weight">
+            <input type="password" placeholder="Confirm Password">
+        </b-modal>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 import { createUser } from '../repository'
+import ImageSlider from './ImageSlider'
 console.log('starting script')
 export default {
   name: 'LoginSignup',
+  components: {
+    ImageSlider
+  },
   data () {
     return {
       firstName: '',
--- barbuddies/src/router/index.js
+++ barbuddies/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import LoginSignup from '@/components/LoginSignup'
+import homepage from '@/components/Homepage'
+import Groups from '@/components/Groups'
+
+
 
 Vue.use(Router)
 
@@ -8,8 +12,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'LoginSignup',
+      component: LoginSignup
+    },
+    {
+      path: '/homepage/',
+      name: 'homepage',
+      component: HomePage
+    },
+    {
+      path: '/groups/',
+      name: 'Groups',
+      component: Groups
     }
   ]
 })
