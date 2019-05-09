9fa39914362b30ca5b3cc331a374e21887be02b3
Irene Hsieh <hsiehiisuan@gmail.com>
2019-05-09 13:20:53 -0700

    navbar eslint errors

--- barbuddies/src/components/NavBar.vue
+++ barbuddies/src/components/NavBar.vue
@@ -25,19 +25,19 @@
 </template>
 <script>
 /* eslint-disable */
-    export default {
-        name: "NavBar",
-        methods: {
-            openNav: function() {
-                document.getElementById("mySidebar").style.width = "250px";
-                document.getElementById("main").style.marginLeft = "250px";
-            },
-            closeNav: function() {
-                document.getElementById("mySidebar").style.width = "0";
-                document.getElementById("main").style.marginLeft= "0";
-            }
+export default {
+  name: "NavBar",
+  methods: {
+    openNav: function() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    },
+    closeNav: function() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
     }
-    }         
+  }
+}         
 </script>
 <style scoped>
   body {
