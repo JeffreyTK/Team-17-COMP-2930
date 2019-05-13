9eb73e6f2cd86ced16af2bb250f569097684d738
Luke Hansen <lukejohnhansen@gmail.com>
2019-05-13 16:46:12 -0700

    debugged

--- barbuddies/src/components/FullCalendar.vue
+++ barbuddies/src/components/FullCalendar.vue
@@ -2,14 +2,19 @@
   <div>
     <NavBar />
     <div class='demo-app'>
+      <div class='demo-app-top'>
+        <!-- <button @click="toggleWeekends">toggle weekends</button>
+        <button @click="gotoPast">go to a date in the past</button>
+        (also, click a date/time to add an event) -->
+      </div>
       <FullCalendar
         class='demo-app-calendar'
         ref="fullCalendar"
-        :defaultView="calendarView"
+        defaultView="timeGridWeek"
         :header="{
           left: 'prev,next',
           center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          right: 'timeGridWeek,timeGridDay'
         }"
         :plugins="calendarPlugins"
         :weekends="calendarWeekends"
@@ -22,7 +27,6 @@
 
 <script>
 import NavBar from './NavBar'
-import Calendar from './Calendar'
 import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
@@ -30,12 +34,9 @@ import interactionPlugin from '@fullcalendar/interaction'
 import '@fullcalendar/core/main.css'
 import '@fullcalendar/daygrid/main.css'
 import '@fullcalendar/timegrid/main.css'
-
 export default {
-  name: 'FullCalendar',
   components: {
     NavBar,
-    Calendar,
     FullCalendar // make the <FullCalendar> tag available
   },
   data: function () {
@@ -52,22 +53,13 @@ export default {
     }
   },
   methods: {
-    handleDateClick: function (arg) {
+    handleDateClick (arg) {
       if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
         this.calendarEvents.push({ // add new event data
           title: 'New Workout',
           start: arg.date,
           allDay: arg.allDay
         })
-      }
-    }
-  },
-  mounted: {
-    calendarView: function () {
-      if (Calendar._inactive) {
-        FullCalendar.defaultView = 'timeGridWeek'
-      } else {
-        FullCalendar.defaultView = 'dayGridMonth'
       }
     }
   }
@@ -80,7 +72,7 @@ export default {
 @import '@fullcalendar/timegrid/main.css';
 .demo-app {
   font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-  font-size: 10px;
+  font-size: 12px;
 }
 .demo-app-calendar {
   margin: 0 auto;
