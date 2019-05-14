45eca45c3127a7501905e2cb1296b2b3749cf5f0
Irene Hsieh <hsiehiisuan@gmail.com>
2019-05-14 16:12:31 -0700

    merge conflicts

--- barbuddies/src/components/Homepage.vue
+++ barbuddies/src/components/Homepage.vue
@@ -1,40 +1,27 @@
 <template>
-  <div id="homepage">
-    <NavBar/>
-    <br/><br/><br/><br/>
-    <FullCalendar/>
+  <div class="homepage">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <FullCalendar />
   </div>
 </template>
-
 <script>
-
-/* eslint-disable */
-import FullCalendar from './FullCalendar'
 import NavBar from './NavBar'
+import FullCalendar from './FullCalendar'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-
 export default {
-  name: 'homepage',
+  name: 'Groups',
   components: {
     NavBar,
     FullCalendar
   }
 }
 </script>
-
 <style scoped>
-#homepage {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 0px;
-  margin-left: 0px;
-}
-img {
-  width: 200px;
-}
+
 </style>
--- barbuddies/src/router/index.js
+++ barbuddies/src/router/index.js
@@ -39,7 +39,7 @@ export default new Router({
     },
     {
       path: '/profile/',
-      name: 'Profile',
+      name: 'profile',
       component: Profile
     },
     {
