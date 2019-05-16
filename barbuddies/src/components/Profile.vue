8c3652c754d08a1bafddf27cc0616496453704cc
Irene Hsieh <hsiehiisuan@gmail.com>
2019-05-16 13:04:03 -0700

    merge conflict

--- barbuddies/src/components/Profile.vue
+++ barbuddies/src/components/Profile.vue
@@ -3,21 +3,22 @@
     <NavBar/>
     <br/><br/><br/>
     <div class="profile">
-      <br />
+      <br /><br /><br /><br /><br />
       <img id="avatar" src="https://dummyimage.com/200x200/000/fff" class="rounded-circle">
       <h2>{{firstName}}</h2>
       <h3>{{email}}</h3>
       <br/>
+      <br/>
       <form>
         <div class="form-group row">
-          <label for="dob" class="col-sm-2 col-form-label">Date of Birth</label>
-          <div class="col-sm-2">
+          <label for="dob" class="col-sm-5 col-form-label">Date of Birth</label>
+          <div class="col-sm-5">
             <input type="date" class="form-control" id="dob" placeholder="Date of Birth">
           </div>
         </div>
         <div class="form-group row">
-          <label for="gender" class="col-sm-2 col-form-label">Gender</label>
-          <div class="col-sm-2">
+          <label for="gender" class="col-sm-5 col-form-label">Gender</label>
+          <div class="col-sm-5">
             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
               <option selected>Choose</option>
               <option value="1">Female</option>
@@ -27,33 +28,31 @@
             </select>
           </div>
         </div>
-
         <div class="form-group row">
-          <label for="weight" class="col-sm-2 col-form-label">Weight (lbs)</label>
-          <div class="col-sm-2">
+          <label for="weight" class="col-sm-5 col-form-label">Weight (lbs)</label>
+          <div class="col-sm-5">
             <input type="number" class="form-control" id="weight" placeholder="Weight" min="0" max="1000">
           </div>
         </div>
         <div class="form-group row">
-          <label for="height" class="col-sm-2 col-form-label">Height</label>
-          <div class="col-sm-2">
+          <label for="height" class="col-sm-5 col-form-label">Height</label>
+          <div class="col-sm-5">
             <input type="number" class="form-control" id="height" placeholder="Height" min="0" max="1000">
           </div>
         </div>
         <div class="form-group row">
-          <label for="password" class="col-sm-2 col-form-label">Password</label>
-          <div class="col-sm-2">
+          <label for="password" class="col-sm-5 col-form-label">Password</label>
+          <div class="col-sm-5">
             <input type="password" class="form-control" id="dob" placeholder="Change Password">
           </div>
         </div>
         <br />
-        <b-button variant="outline-dark" v-on:click="save">Save</b-button>
+        <button v-on:click="save">Save</button>
       </form>
     </div>
   </div>
 
 </template>
-
 <script>
 import { updateUser } from '../repository'
 import NavBar from './NavBar'
@@ -78,28 +77,29 @@ export default {
     }
   },
   methods: {
-    save: function () {
+    /* save: function () {
       updateUserProfile(id).then((data) => {
-        data.DOB = this.DOB,
-        data.gender = this.Gender,
-        data.weight = this.weight,
-        data.height = this.height,
+        data.DOB = this.DOB
+        data.gender = this.Gender
+        data.weight = this.weight
+        data.height = this.height
         data.password = this.password
       })
-    }
+    } */
   },
   mounted () {
     let id = '5cdb23cecb38bb9baed28ac2'
     updateUser(id).then((data) => {
-      this.firstName = data.firstName,
-      this.email = data.email,
+      this.firstName = data.firstName
+      this.email = data.email
       this.DOB = data.DOB
-      this.Gender = data.gender,
-      this.weight = data.weight,
-      this.height = data.height,
-      this.email = data.email,
+      this.Gender = data.gender
+      this.weight = data.weight
+      this.height = data.height
+      this.email = data.email
       this.password = data.password
     })
   }
 }
+
 </script>
