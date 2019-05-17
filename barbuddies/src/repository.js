774b578d62a1059582b6a3f99b2f5e9a07a36e5b
Luke Hansen <lukejohnhansen@gmail.com>
2019-05-17 16:00:45 -0700

    SERVER UP

--- barbuddies/src/repository.js
+++ barbuddies/src/repository.js
@@ -2,11 +2,11 @@
 
 import axios from 'axios'
 // the base url is the localhost at which the database is stored
-const BASE_URL = '159.203.57.79:5000'
+const BASE_URL = 'http://159.203.57.79:5000'
 export function createUser (data) {
   // on the post call if the url is this
   // sets the data of first name to the first name and responds with the data to the database
-  return axios.post(`${BASE_URL}/api/user/create`,
+  axios.post(`${BASE_URL}/api/user/create`,
     {
       firstName: data.firstName,
       lastName: data.lastName,
@@ -19,10 +19,13 @@ export function createUser (data) {
       userID: data.userID,
       groupID: data.groupID
     }).then(response => {
-    return response.data
-  })
+      console.log(response);
+      return response.data
+    }).catch(err => {
+      console.error(err);
+    })
   // on error will push out the error message
-    .catch(err => Promise.reject(err.message))
+    //.catch(err => Promise.reject(err.message))
 }
 export function createGroup (data) {
   return axios.post(`${BASE_URL}/api/group/create`,
